ac7b4747561aaf2424fd1e15226d16d034529c9d
Subashreebalakrishnan <107244820+Subashreebalakrishnan@users.noreply.github.com>
2022-06-20 21:11:05 +0530

    Add files via upload

---
+++ question2.js
@@ -0,0 +1,9 @@
+var request = new XMLHttpRequest();
+request.open('GET','https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json');
+request.send();
+request.onload = function(){
+var countrydata=JSON.parse(request.response);
+for(let output of countrydata){
+console.log(output.flag)
+}
+}   
